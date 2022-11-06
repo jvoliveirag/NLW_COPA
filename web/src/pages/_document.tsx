b82a285403abfe5b61f4750854afb9d80fe9f76a
jvoliveirag <jv.oliveira@gmail.com>
2022-11-06 02:19:13 -0300

    Ajuste no arquivo de configuração da página para rodar a build

--- web/src/pages/_document.tsx
+++ web/src/pages/_document.tsx
@@ -5,7 +5,6 @@ export default function Document() {
         <Html>
             <Head>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
                 <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;1,700&display=swap" rel="stylesheet" />
             </Head>
             <body className='bg-gray-900 bg-app bg-no-repeat bg-cover'>
